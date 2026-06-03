3cbf4033363ef93d23898adb13465cf33cf12a66
Ashutosh Pradhan <flux4rray@gmail.com>
2026-06-03 17:45:19 +0530

    hmm

--- template.cpp
+++ template.cpp
@@ -105,7 +105,7 @@ void solve() {
 }
 
 signed main() {
-//hmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+//hmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     int t = 1;
